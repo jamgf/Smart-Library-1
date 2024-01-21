886135c08705f3608cce796f872e9cf544861cbb
jamgf <jmfilh@gmail.com>
2024-01-20 19:30:40 -0500

    20-01-24

--- mainnew.py
+++ mainnew.py
@@ -24,13 +24,13 @@ class LMS:
         for key, value in self.books_dict.items():
             print(key, "\t\t", value.get("books_title"),"- [",value.get("Status"),"]")
 
-    def Issue_books(self):
+    def issue_books(self):
         books_id = input("Enter books ID: ")
         current_date = datetime.datetime.now().strftime("%Y-%m_%d %H:%M:%S")
         if books_id in self.books_dict.keys():
             if self.books_dict[books_id]["Status"] == "Available":
                 print(f"This book is already issued to {self.books_dict[books_id]['lender_name']} on {self.books_dict[books_id]['Issue_date']}")
-                return self.Issue_books()
+                return self.issue_books()
             elif self.books_dict[books_id]['Status'] == "Available":
                 your_name = input("Enter your name: ")
                 self.books_dict[books_id]['lender_name'] = your_name
@@ -39,7 +39,64 @@ class LMS:
                 print("Book Issued Successfully!")
         else:
             print("Book ID not found!")
-            return self.Issue_books()
+            return self.issue_books()
         
+    def add_books(self):
+        new_books = input("Enter books title:")
+        if new_books == "":
+            return self.add_books()
+        elif len(new_books) > 25:
+            print("Books title length is too long. Title length should be 20 chars")
+            return self.add_books()
+        else:
+            with open(self.list_of_books,"a") as bk:
+                bk.writelines(f"{new_books}\n")
+                self.books_dict.update({str(int(max(self.books_dict))+1):{'books_title':new_books,'lender_name':'',
+                'Issue_date':'', 'Status':'Available'}})
+                print(f"This books '{new_books}' has been added successfully")
+
+
+    def return_books(self):
+        books_id = input('Enter books Id: ')
+        if books_id in self.books_dict.keys():
+            if self.books_dict[books_id]['Status'] == 'Available':
+                print('This book is already available in library')
+                return return_books()
+            elif not self.books_dict[books_id]['Status'] == 'Available':
+                self.books_dict[books_id]['lender_name'] = ''
+                self.books_dict[books_id]['Issue_date'] = ''
+                self.books_dict[books_id]['Status'] = 'Available'
+                print('Successfully updated')
+            else:
+                print('Book ID is not found \n')
+
+try:
+    myLMS = LMS('list_of_books.txt', 'Python')
+    press_key_list = {"D": "Display books","I": "Issue books", "A":"Add books",
+                      "R":"Return books", "Q":"Quit program"}
+    key_press = False
+    while not (key_press == "q"):
+        print(f"\n---------Welcome to {myLMS.library_name} --------")
+        for key,value in press_key_list.items():
+            print("Press", key, "To", value)
+            key_press = input("Press key: ").lower()
+            if key_press == "i":
+                print("\nCurrent selection: Issue books")
+                myLMS.issue_books()
+            elif key_press == "a":
+                print("\nCurrent selection: Add books")
+                myLMS.add_books()
+            elif key_press == "d":
+                print("\nCurrent selection: Display books")
+                myLMS.display_books()
+            elif key_press == "r":
+                print("\nCurrent selection: Return books")
+                myLMS.return_books()
+            elif key_press == "q":
+                break
+            else:
+                continue
+except Exception as e:
+    print("Something went wrong. Choose again")
 program = LMS("list_of_books.txt","Python's Library")
 print(program.display_books())
